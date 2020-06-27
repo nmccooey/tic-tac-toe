f52265fff7b78e8ec986d7f2662579d87814feec
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-06-27 10:52:56 -0400

    Updated UI, current favicon removed - going to change it.

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,25 @@
-const newGameButton = document.querySelector(".new-game");
-newGameButton.addEventListener("click", startGame);
 
-function startGame(){
-    newGameButton.disabled = true;
+function createBoard(){
 
     //Create board.
-    const gameContainer = document.querySelector(".game-container");
+    const gameGrid = document.querySelector(".grid");
     for (let i = 0; i < 9; i++) {
         const gameSpot = document.createElement("div");
         gameSpot.className = "game-spot";
-        gameContainer.appendChild(gameSpot);
+        gameSpot.setAttribute("data-index", i);
+        gameGrid.appendChild(gameSpot);
     }
+
+    startGame();
 }
+
+function startGame() {
+
+    document.querySelectorAll('.game-spot').forEach(gameSpot => {
+        gameSpot.addEventListener('click', event => {
+            gameSpot.innerHTML = "X";
+        });
+    });
+}
+
+createBoard();
